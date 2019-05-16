ab67c598ec4dd50be3de3f34cb2d37bf36ac0c3a
Jenna Ransom <jenna.ransom@gmail.com>
2019-05-16 05:37:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,23 @@
 class Song 
   attr_accessor :name, :artist, :genre
-  
   @@count = 0 
+  @@genre = [ ]
+  
   def initialize (name, artist, genre)
     @name = name 
     @artist = artist
     @genre = genre
     @@count += 1 
+    @@genre << genre
   end
   
   def self.count
     @@count
   end
+  
+  def self.genres 
+    @@genre.
+  end
+  
   
 end
